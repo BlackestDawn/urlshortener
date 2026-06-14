2df037d485f5f36d4dd50fc567c5701c512b4831
Alexander Stauch <blacke4dawn@gmail.com>
2026-06-14 07:07:01 +0200

    adding repository base and CreateShortUrl

---
+++ internal/repository/postgresRepository.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/BlackestDawn/urlshortener/config"
+	"github.com/BlackestDawn/urlshortener/internal/domain"
+)
+
+type PostgresRepository struct {
+	QBQueries *Queries
+}
+
+func NewPGRepository(cfg config.Config) (*PostgresRepository, error) {
+	repo := new(PostgresRepository)
+
+	db, err := sql.Open("postgres", cfg.DBUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	repo.QBQueries = New(db)
+
+	return repo, nil
+}
+
+func (r *PostgresRepository) CreateShortUrl(url string) (*domain.ShortUrl, error) {
+	if res, err := domain.ValidateURL(url); !res {
+		return nil, fmt.Errorf("Invalid URL: %w", err)
+	}
+
+	code, err := domain.GenerateCode(url)
+	if err != nil {
+		return nil, fmt.Errorf("Error generating code: %w", err)
+	}
+
+	entry, err := r.QBQueries.CreateShortUrl(context.Background(), CreateShortUrlParams{
+		Code:        code,
+		OriginalUrl: url,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return entryToDomain(entry), nil
+}
