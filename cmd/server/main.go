db225381a0a1d788ff83fc3c8aaeff08c11d9924
Alexander Stauch <blacke4dawn@gmail.com>
2026-06-29 09:40:37 +0200

    adding gracefull shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/BlackestDawn/urlshortener/config"
 	"github.com/BlackestDawn/urlshortener/internal/repository"
@@ -41,5 +47,29 @@ func main() {
 		}
 	}
 
-	router.Run(cfg.Port)
+	server := http.Server{
+		Addr:    cfg.Port,
+		Handler: router,
+	}
+
+	go func() {
+		log.Printf("Starting server on %s\n", cfg.Port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Listen error: %s", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown signal recieved")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Forced shutdown: %s", err)
+	}
+
+	log.Println("Server exited gracefully")
 }
